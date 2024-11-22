77305258651b4cca002c774cfe1e1b3ee2c245da
farreel17 <rajendrafarrel55@gmail.com>
2024-11-22 14:23:01 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
   </head>
   <body>
     <h1>Hello World</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore pariatur placeat culpa! Amet, quam in officia similique reprehenderit impedit, dolor molestiae aut, ducimus neque error ad non pariatur eligendi voluptatum.</p>
   </body>
 </html>
